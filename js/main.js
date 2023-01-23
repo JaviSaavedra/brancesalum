550ef49faadcef560ab76e16e63c9eb2bc89805f
JaviSaavedra <javierasaavedra95@outlook.com>
2023-01-23 09:48:35 -0300

    ver cambios

--- js/main.js
+++ js/main.js
@@ -10,3 +10,41 @@
     document.getElementById("fechahorazonahoraria").innerHTML = time;
   }
   setInterval(displayTime, 1000)
+
+//  funcion para fancybox y hora
+ 
+$(document).ready(function() {
+  $('.fancybox').fancybox();
+});
+
+function displayTime() {
+  var time = new Date()
+  document.getElementById("fechahorazonahoraria").innerHTML = time;
+}
+setInterval(displayTime, 1000)
+
+
+//  funcion para fancybox y hora
+ 
+$(document).ready(function() {
+  $('.fancybox').fancybox();
+});
+
+function displayTime() {
+  var time = new Date()
+  document.getElementById("fechahorazonahoraria").innerHTML = time;
+}
+setInterval(displayTime, 1000)
+
+
+//  funcion para fancybox y hora
+ 
+$(document).ready(function() {
+  $('.fancybox').fancybox();
+});
+
+function displayTime() {
+  var time = new Date()
+  document.getElementById("fechahorazonahoraria").innerHTML = time;
+}
+setInterval(displayTime, 1000)
